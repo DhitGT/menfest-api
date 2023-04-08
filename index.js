8cdb5bbd241cbea495063bdca34c0a541708ceb0
DhitGT <104990882+DhitGT@users.noreply.github.com>
2023-04-08 12:17:16 +0700

    change port 1

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
 
 const app = express();
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
 
-app.listen(process.env.PORT, () => console.log('server is running '));
+app.listen(port, () => console.log('server is running '));
